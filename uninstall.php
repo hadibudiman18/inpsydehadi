858b568ecabc039bbe565067e50c26e56b120966
hadibudiman18 <67779048+hadibudiman18@users.noreply.github.com>
2020-07-16 05:08:37 +0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,27 +1,4 @@
 <?php
-/**
- * PHP version 7
- *
- * Uninstall file
- *
- * @category  Custom
- * @package   Inpsydehadi
- * @author    Hadi Budiman <hadibudiman@gmail.com>
- * @copyright 2020 Hadi Budiman
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt GPL-2.0-or-later
- * @link      http://hadibudiman.site/
- *
- * @wordpress-plugin
- * Plugin Name: Inpsydehadi
- * Plugin URI:  http://hadibudiman.site/
- * Description: test plugin
- * Version:     1.0.0
- * Author:      Hadi Budiman
- * Author URI:  http://hadibudiman.site/
- * License:     GPL v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: Inpsydehadi
- */
 
 defined('WP_UNINSTALL_PLUGIN') || exit;
 
